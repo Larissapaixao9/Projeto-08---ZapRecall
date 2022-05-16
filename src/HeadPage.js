8c50fed5158e4e9219249bd183cf23e4516a1a77
Larissa Paixao <laryssapaixao9@gmail.com>
2022-05-16 17:41:04 -0300

    alteracao de CSS e variaveis

--- src/HeadPage.js
+++ src/HeadPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 export default function HeadPage(){
     return(
-    <div className='DisplayFlex'>
+    <div className='DisplayFlex margin'>
         <img src='image 1.png' />
         <h1>ZapRecall</h1>
 </div>
--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home({setInit}){
     return (
     <>
-        <div className="AlingCenter">
+        <div className="AlingCenter1">
         <div><img src="./image 1.png" alt="logo"/></div>
         <div><h1>ZapRecall</h1></div>
         <div onClick={()=>setInit(true)} className="StartButton BackgroungWhite">Iniciar Recall!</div>
